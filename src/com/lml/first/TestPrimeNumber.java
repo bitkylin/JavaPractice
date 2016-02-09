42f1994e4e4fe634db809789b05b6e0fcffc7d82
lml08 <limingliang2004@126.com>
2016-02-09 14:46:57 +0800

    “D:developerProjectJavafirst_

---
+++ src/com/lml/first/TestPrimeNumber.java
@@ -0,0 +1,28 @@
+package com.lml.first;
+
+public class TestPrimeNumber {
+	/**
+	 * 生成所有的素数；izu存储生成的数组；i是检索素数的范围
+	 */
+	public static void main(String args[]) {
+		System.out.println(2);
+		int[] izu = new int[9999];
+		izu[0] = 2;
+		int j = 1;
+		for (int i = 3; i <= 99999; i++) {
+			boolean judge = false;
+			for (int z = 0; z < j; z++) {
+				if (i % izu[z] == 0) {
+					judge = true;
+					break;
+				}
+			}
+			if (judge == false) {
+				izu[j++] = i;
+				System.out.println(i);
+			}
+		}
+		System.out.println("一共有" + j + "个数字");
+	}
+
+}
---
+++ src/com/lml/first/TestPrimeNumber2.java
@@ -0,0 +1,19 @@
+package com.lml.first;
+
+public class TestPrimeNumber2 {
+	/**
+	 * 生成图案；LINE_NUM为生成图案的行数
+	 */
+	public static void main(String args[]) {
+		final int LINE_NUM = 10;
+		for (int i = 1; i <= LINE_NUM; i++) {
+			for (int j = LINE_NUM - i; j > 0; j--) {
+				System.out.print(" ");
+			}
+			for (int j = 0; j < i; j++) {
+				System.out.print("* ");
+			}
+			System.out.println("");
+		}
+	}
+}
