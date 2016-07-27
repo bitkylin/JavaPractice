ad51acfb91a3b402ec4c935551375663814ad389
123lml123 <limingliang2004@126.com>
2016-07-27 22:08:43 +0800

    添加工程：实现本地文件的简单复制

---
+++ IO/src/IOoperation.java
@@ -0,0 +1,20 @@
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+public class IOoperation {
+
+	public IOoperation() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void copyText(InputStream src, OutputStream dest) throws IOException {
+		try (InputStream input = src; OutputStream output = dest) {
+			byte[] data = new byte[1024];
+			int length;
+			while ((length = input.read(data)) != -1) {
+				output.write(data, 0, length);
+			}
+		}
+	}
+}
--- README.md
+++ README.md
@@ -1,5 +1,16 @@
 # Java简单练习项目
 本项目用于Java程序的练习，内含多个Java工程，会时常更新。
-##1. lmlLinkedList
+## 1. lmlLinkedList
 独立实现Java链表
 
+## 2. IO
+### 1. IOoperation.copyText
+简单的从输入流读取数据输出至输出流
+### 2. test
+简单的文件复制程序，命令行操作指令：
+```cmd
+java test A B
+```
+*其中：A为源文件地址，B为目标地址
+例：java test D:\lml.txt D:\lml2.txt
+*
